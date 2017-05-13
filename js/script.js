5baabc72d5b56bef82284ee471df9ed2a50663d6
chaoya <chaoya@chaoyadeMBP.wv.cc.cmu.edu>
2017-05-13 16:58:59 -0400

    test1

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@ var room_num = 1;
 var current_pressed = 0;
 var user_name = "NULL";
 
+interface RoundUpdate {
+	round: number;
+	time: number;
+}
+
+function PlayerInput(apg: APGsys) : void {
+	apg.setHandlers(new NetworkMessageHandler().Add<RoundUpdate>("time", p => {}));
+}
+
 $(document).ready(function() {
 	$('#username').hide();
 
